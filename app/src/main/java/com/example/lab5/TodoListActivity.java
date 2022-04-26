2b9ec294c7406031a8ebb83fcff89ff46d43d2e3
mjalbert99 <mjalbert@ucsd.edu>
2022-04-25 18:57:51 -0700

    Act 2

--- app/src/main/java/com/example/lab5/TodoListActivity.java
+++ app/src/main/java/com/example/lab5/TodoListActivity.java
@@ -1,6 +1,8 @@
 package com.example.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -8,14 +10,20 @@ import android.util.Log;
 import java.util.List;
 
 public class TodoListActivity extends AppCompatActivity {
-
+    public RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
 
-        List<TodoListItem> todos = TodoListItem.loadJSON(this, "demo_todos.json");
-        Log.d("TodoListActivity", todos.toString());
+        TodoListAdapter adapter = new TodoListAdapter();
+        adapter.setHasStableIds(true);
+
+        recyclerView = findViewById(R.id.todo_items);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        adapter.setTodoListItems(TodoListItem.loadJSON(this,"demo_todos.json"));
     }
 
 
